bf15f06fd282ee130ae64c82710a22d168b0fc13
jpdf00 <jpdf00@gmail.com>
2021-02-16 12:31:46 -0300

    Add linters, README, and License

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy 
-  has_many :comments, dependent: :destroy 
-  validates :username, presence: true, length: { in: 4..20}, uniqueness: true 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i 
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  validates :username, presence: true, length: { in: 4..20 }, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
